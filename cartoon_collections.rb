d832da098bea0c100d437f17a570502dce126700
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-14 15:17:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,13 +20,10 @@ long_planeteer_calls(planeteer_calls)
 
 
 potentially_cheesy_items = ["umbrella", "spinach", "cheddar", "helicopter"]
-
 def find_the_cheese(potentially_cheesy_items)
   cheeses = ["gouda", "cheddar", "camembert"]
-
   potentially_cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
 end
-
 find_the_cheese(potentially_cheesy_items)
