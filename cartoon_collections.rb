77828324def38368904d95e08c6aaeeccb6397fd
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-14 15:08:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
-dwarves = %w[Doc Dopey Bashful Grumpy Sneezy Sleepy Happy]
+dwarves = %w["Doc", "Dopey", "Bashful", "Grumpy", "sneezy", "Sleepy", "Happy"]
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
   end
 end
 roll_call_dwarves(dwarves)
-planeteer_calls = %w[earth wind fire water heart]
+planeteer_calls = %w["earth", "wind" ,"fire" ,"Water", "heart"]
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
